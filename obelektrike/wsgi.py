42c6bae57046e6bc02bb72ce49f4454dc79628d4
Vsevolod Dudakov <vsdudakov@gmail.com>
2018-03-31 12:39:11 +0300

    Fix

--- obelektrike/wsgi.py
+++ obelektrike/wsgi.py
@@ -8,9 +8,13 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
 import os
+import environ
 
 from django.core.wsgi import get_wsgi_application
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+environ.Env.read_env(os.path.join(BASE_DIR, 'env.prod'))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "obelektrike.settings")
 
 application = get_wsgi_application()
