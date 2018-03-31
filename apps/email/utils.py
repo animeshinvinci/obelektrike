c62afceb76403a796b49d09dff7d4927527a3c8f
Vsevolod Dudakov <vsdudakov@gmail.com>
2018-03-31 13:57:15 +0300

    Fix

--- apps/email/utils.py
+++ apps/email/utils.py
@@ -4,10 +4,11 @@ from apps.email.models import Email
 
 
 def send_mail(subject, message, from_email, recipient_list, **kwargs):
-    Email.objects.create(
+    django_send_mail(subject, message, from_email, recipient_list, **kwargs)
+    email = Email(
         from_email=from_email,
         to_emails=', '.join(recipient_list),
         subject=subject,
         body=message,
     )
-    django_send_mail(subject, message, from_email, recipient_list, **kwargs)
+    email.save()
